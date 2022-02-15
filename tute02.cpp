4b3b705be8c4d868f5f7b9cabe0a13b3b9e4996c
IT21177514 <99228728+IT21177514@users.noreply.github.com>
2022-02-15 00:17:03 +0000

    Tutorial-02

--- tute02.cpp
+++ tute02.cpp
@@ -12,33 +12,44 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 int main()
 {
    double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
-   
-   switch (etype) {
-      case 1 :
-          otRate = 1000;
-          break;
-      case 2 :
-          otRate = 1500;
-          break;
-      default :
-          otRate = 1700;
-          break;
-   }
-
-
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
-  
-   return 0;
+ int etype, otHrs, otRate;
+ 
+ //printf("Enter Employee Type : ");
+ //scanf("%d", &etype);
+ cout<<"\n\nEnter employee type: ";
+ cin>>etype;
+ 
+ //printf("Enter Salary : ");
+ //scanf("%f", &salary);
+ cout<<"\n\nEnter salary: ";
+ cin>>salary;
+ 
+ //printf("Enter OtHrs : ");
+ //scanf("%d", &otHrs);
+ cout<<"\n\nEnter OT hours: ";
+ cin>>otHrs;
+ 
+ 
+ switch (etype) {
+ case 1 :
+ otRate = 1000;
+ break;
+ case 2 :
+ otRate = 1500;
+ break;
+ default :
+ otRate = 1700;
+ break;
+ }
+ netSalary = salary + otHrs* otRate;
+ 
+ //printf("Net Salary is %f ", netSalary);
+cout<<"\n\nNet salary is "<<netSalary;	
+	
+	 return 0;
 }
--- tute04.cpp
+++ tute04.cpp
@@ -12,7 +12,7 @@ long nCr(int n, int r);
 Do not modify the main function.*/
 
 #include <iostream>
-
+using namespace std;
 long Factorial(int no);
 long nCr(int n, int r);
 
@@ -27,3 +27,54 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long nCr(int n,int r){
+	
+	
+	long factorial_n;
+	long factorial_r;
+	long factorial_nr;
+	long ncr;
+	
+	int counter;
+	
+	//initialization phase
+	factorial_n=1;
+	factorial_r=1;
+	factorial_nr=1;
+	ncr=0;
+	
+	//finding factoral of n
+	for(counter=1;counter<=n;counter++){
+		
+		factorial_n=factorial_n*counter;
+		
+	}//end for loop
+	
+	
+	
+	//finding factorial of r
+	for(counter=1;counter<=r;counter++){
+		
+		factorial_r=factorial_r*counter;
+		
+	}//end for loop
+	
+	
+	//finding factorial of (n-r)
+	for(counter=1;counter<=(n-r);counter++){
+		
+		factorial_nr=factorial_nr*counter;
+		
+	}//end for loop
+	
+	
+	
+	
+	//finding value of ncr
+	ncr=factorial_n/(factorial_r*factorial_nr);
+	
+	return ncr;
+	
+	
+}//end function nCr
+
